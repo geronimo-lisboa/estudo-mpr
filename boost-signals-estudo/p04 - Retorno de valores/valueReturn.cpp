6b2ae17d9f01e584dd391cafe8db227f99bfb9e2
luciano <luciano.geronimo.fnord@gmail.com>
2017-09-13 15:51:11 -0300

    Exemplo de multiplos slots por sinal.

---
+++ boost-signals-estudo/p04 - Retorno de valores/valueReturn.cpp
@@ -0,0 +1,42 @@
+#include <boost\signals2.hpp>//O header da boost. Esse header precisa que sua lib seja inclusa.
+#include <iostream>
+//Essa classe trata dois signals
+class Bagulho{
+private:
+	const int id;
+public:
+	Bagulho(int i) : id(i){
+	}
+	//Tratando um tipo de signal
+	void operator()(){
+		std::cout << "Slot tipo 1" <<" bagulho id = "<<id<< std::endl;
+	}
+	//Tratando outro tipo de signal
+	void operator()(int x){
+		std::cout << "Slot tipo 2, param = "<<x <<" bagulho id = "<<id<< std::endl;
+	}
+};
+int main(int argc, char** argv){
+	//Dois tipos de sinal
+	boost::signals2::signal<void()> sinalTipo1;
+	boost::signals2::signal<void(int)> sinalTipo2;
+	Bagulho *bagulho0 = new Bagulho(0);
+	Bagulho *bagulho1 = new Bagulho(1);
+	Bagulho *bagulho2 = new Bagulho(2);
+	//Conecta os slots
+	//Não posso passar o ponteiro mas sim a referência para o valor apontado pelo ponteiro, então uma dereferência basta.
+	sinalTipo1.connect(2,*bagulho0);
+	sinalTipo2.connect(2,*bagulho0);
+
+	sinalTipo1.connect(0,*bagulho1);
+	sinalTipo2.connect(0,*bagulho1);
+
+	sinalTipo1.connect(1, *bagulho2);
+	sinalTipo2.connect(2, *bagulho2);
+	//Chama.
+	sinalTipo1();
+	sinalTipo2(10);
+	return 0;
+}
+
+
